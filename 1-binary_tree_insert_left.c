fb73627529e9e92cb04f0f2e4910352fb8a6bfd3
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-01-08 18:00:47 +0000

    fix betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new_node->right = NULL;
-	
+
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
